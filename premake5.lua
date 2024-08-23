2cf17d15cf04dd542b2b4becd645650dbce5eb86
Michael Mathu <michaelmathu6@gmail.com>
2024-08-23 11:59:17 +0300

    premake

--- premake5.lua
+++ premake5.lua
@@ -1,19 +1,37 @@
 project "oatpp-postgresql"
     kind "StaticLib"
     language "C++"
+    cppdialect "C++20"
+    staticruntime "on"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
     files {
-        "src/oatpp/**.hpp",
-        "src/oatpp/**.cpp"
+        "src/oatpp-postgresql/**.hpp",
+        "src/oatpp-postgresql/**.cpp"
+    }
+
+    includedirs {
+        "src",
+        "../oatpp/src"
     }
 
     defines {
         "OATPP_DISABLE_ENV_OBJECT_COUNTERS=ON",
         "OATPP_BUILD_TESTS=OFF"
     }
+    
+    links {
+        "oatpp"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+        includedirs {
+        "C:/Program Files/PostgreSQL/16/include"
+        }
 
     filter "configurations:Debug"
 		runtime "Debug"
